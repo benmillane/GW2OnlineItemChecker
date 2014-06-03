67667afc4602f32eaccc5cb0bd3c85c6497b4f6c
Ben Millane <benmillane@gmail.com>
2014-06-03 20:44:49 +0100

    Continued the painstaking task of setting up entities and relationships and navigation properties.

--- GW2OICUpdater/GW2OIC.GW2APIJSONDomain/EF Classes/EFBackTypeInfo.cs
+++ GW2OICUpdater/GW2OIC.GW2APIJSONDomain/EF Classes/EFBackTypeInfo.cs
@@ -6,36 +6,92 @@ using System.Threading.Tasks;
 
 namespace GW2OIC.GW2APIJSONDomain.EF_Classes
 {
+    /// <summary>
+    /// Belongs to a EFGW2Item (single/optional)
+    /// </summary>
     public class EFBackTypeInfo
     {
-        public BackInfusion_Slot[] infusion_slots { get; set; }
-        public BackInfixUpgrade infix_upgrade { get; set; }
+        public int EFBackTypeInfoID { get; set; } //PK
+
         public int suffix_item_id { get; set; }
         public int? secondary_suffix_item_id { get; set; }
+
+        //FK
+        public int EFGW2ItemID { get; set; }
+
+        //Navigation Propeties
+        public virtual List<BackInfusion_Slot> infusion_slots { get; set; }
+        public virtual BackInfixUpgrade infix_upgrade { get; set; }
+        public virtual EFGW2Item EFGW2Item { get; set; }
     }
 
+    /// <summary>
+    /// Belongs to a EFBackTypeInfo (many)
+    /// </summary>
     public class BackInfusion_Slot
     {
+        public int BackInfusion_SlotID { get; set; } //PK
         public int item_id { get; set; }
-        public string[] flags { get; set; }
+
+        //FK
+        public int EFBackTypeInfoID { get; set; }
+
+        //Navigation Properties
+        public virtual List<string> flags { get; set; }
+        public virtual EFBackTypeInfo EFBackTypeInfo { get; set; }
+
     }
 
+    /// <summary>
+    /// Belongs to a EFBackTypeInfo (single)
+    /// </summary>
     public class BackInfixUpgrade
     {
+        public int BackInfixUpgradeID { get; set; } //PK
         public BackBuff buff { get; set; }
         public BackAttribute[] attributes { get; set; }
+
+        //FK
+        public int EFBackTypeInfoID { get; set; }
+
+        //Navigation Properties
+        public virtual BackBuff buff { get; set; }
+        public virtual List<BackAttribute> attributes { get; set; }
+        public virtual EFBackTypeInfo EFBackTypeInfo { get; set; }
     }
 
+    /// <summary>
+    /// Belongs to a BackInfixUpgrade (single)
+    /// </summary>
     public class BackBuff
     {
+        public int BackBuffID { get; set; } //PK
         public int skill_id { get; set; }
         public string description { get; set; }
+
+        //FK
+        public int BackInfixUpgradeID { get; set; }
+
+        //Navigation Properties
+        public virtual BackInfixUpgrade BackInfixUpgrade { get; set; }
+
     }
 
+    /// <summary>
+    /// Belongs to a BackInfixUpgrade (many)
+    /// </summary>
     public class BackAttribute
     {
+        public int BackAttributeID { get; set; } //PK
         public string attribute { get; set; }
         public int modifier { get; set; }
+
+        //FK
+        public int BackInfixUpgradeID { get; set; }
+
+        //Navigation Properties
+        public virtual BackInfixUpgrade BackInfixUpgrade { get; set; }
+
     }
 
 }
--- GW2OICUpdater/GW2OIC.GW2APIJSONDomain/EF Classes/EFGW2Item.cs
+++ GW2OICUpdater/GW2OIC.GW2APIJSONDomain/EF Classes/EFGW2Item.cs
@@ -23,18 +23,18 @@ namespace GW2OIC.GW2APIJSONDomain.EF_Classes
         public string[] flags { get; set; }
         public string[] restrictions { get; set; }
 
-        //FK's
-        public int? EFArmourTypeInfoID { get; set; }
-        public int? EFBackTypeInfoID { get; set; }
-        public int? EFBagTypeInfoID { get; set; }
-        public int? EFConsumableTypeInfoID { get; set; }
-        public int? EFContainerTypeInfoID { get; set; }
-        public int? EFGatheringTypeInfoID { get; set; }
-        public int? EFGizmoTypeInfoID { get; set; }
-        public int? EFToolTypeInfoID { get; set; }
-        public int? EFTrinketTypeInfoID { get; set; }
-        public int? EFUpgradeComponentTypeInfoID { get; set; }
-        public int? EFWeaponTypeInfoID { get; set; }
+        ////FK's
+        //public int? EFArmourTypeInfoID { get; set; }
+        //public int? EFBackTypeInfoID { get; set; }
+        //public int? EFBagTypeInfoID { get; set; }
+        //public int? EFConsumableTypeInfoID { get; set; }
+        //public int? EFContainerTypeInfoID { get; set; }
+        //public int? EFGatheringTypeInfoID { get; set; }
+        //public int? EFGizmoTypeInfoID { get; set; }
+        //public int? EFToolTypeInfoID { get; set; }
+        //public int? EFTrinketTypeInfoID { get; set; }
+        //public int? EFUpgradeComponentTypeInfoID { get; set; }
+        //public int? EFWeaponTypeInfoID { get; set; }
 
         //Navigation Properties
         public virtual EFArmourTypeInfo armor { get; set; }
